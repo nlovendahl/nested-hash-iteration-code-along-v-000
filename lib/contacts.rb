801225aa1b69844a211d2a6779746b4e9ae56cac
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-22 03:35:29 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,6 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, data|
     data.each do |attribute, value|
+      if attribute == :favorite_ice_cream_flavors
         data.delete_if{|ice_cream| ice_cream == "strawberry"}
       end
     end
